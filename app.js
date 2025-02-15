dbeadf9507a0fa9cbf76cc9b23bd2b4cc510e37b
Vishal sharma <vishalsharma42619@gmail.com>
2025-02-15 13:17:59 +0530

    Basic structure of Website

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('views', path.join(__dirname, 'views')); // Ensure views folder path is correct
+
+app.get('/', (req, res) => {
+    res.render('index'); // Render the index.ejs file
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Server running on http://localhost:${3000}`));
